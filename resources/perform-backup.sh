b3bed9fb0491ba91275d8d8dc217042a3ff451c0
hansol <jhs313@kinx.net>
2022-05-20 09:20:32 +0900

    add_date

--- resources/perform-backup.sh
+++ resources/perform-backup.sh
@@ -3,29 +3,29 @@
 
 # Set the has_failed variable to false. This will change if any of the subsequent database backups/uploads fail.
 has_failed=false
-
+current_date=$(TZ='Asia/Seoul' date +%y%m%d%H%M%S)
 
 # Loop through all the defined databases, seperating by a ,
 for CURRENT_DATABASE in ${TARGET_DATABASE_NAMES//,/ }
 do
-
+BACKUPFILES="$CURRENT_DATABASE"_"backup"_"$current_date"
     # Perform the database backup. Put the output to a variable. If successful upload the backup to S3, if unsuccessful print an entry to the console and the log, and set has_failed to true.
-    if sqloutput=$(mysqldump -u $TARGET_DATABASE_USER -h $TARGET_DATABASE_HOST -p$TARGET_DATABASE_PASSWORD -P $TARGET_DATABASE_PORT $CURRENT_DATABASE 2>&1 > /tmp/$CURRENT_DATABASE.sql)
+    if sqloutput=$(mysqldump -u $TARGET_DATABASE_USER -h $TARGET_DATABASE_HOST -p$TARGET_DATABASE_PASSWORD -P $TARGET_DATABASE_PORT $CURRENT_DATABASE 2>&1 > /tmp/$CURRENT_DATABASE_$current_date.sql)
     then
         
-        echo -e "Database backup successfully completed for $CURRENT_DATABASE at $(date +'%d-%m-%Y %H:%M:%S')."
+        echo -e "Database backup successfully completed for $CURRENT_DATABASE at $(TZ='Asia/Seoul' date +'%Y-%m-%d %H:%M:%S')."
 
         # Perform the upload to S3. Put the output to a variable. If successful, print an entry to the console and the log. If unsuccessful, set has_failed to true and print an entry to the console and the log
-        if awsoutput=$(aws s3 cp /tmp/$CURRENT_DATABASE.sql s3://$AWS_BUCKET_NAME$AWS_BUCKET_BACKUP_PATH/$CURRENT_DATABASE.sql 2>&1)
+        if awsoutput=$(aws s3 cp /tmp/$CURRENT_DATABASE_$current_date.sql s3://$AWS_BUCKET_NAME$AWS_BUCKET_BACKUP_PATH/$BACKUPFILES.sql 2>&1)
         then
-            echo -e "Database backup successfully uploaded for $CURRENT_DATABASE at $(date +'%d-%m-%Y %H:%M:%S')."
+            echo -e "Database backup successfully uploaded for $CURRENT_DATABASE at $(TZ='Asia/Seoul' date +'%Y-%m-%d %H:%M:%S')."
         else
-            echo -e "Database backup failed to upload for $CURRENT_DATABASE at $(date +'%d-%m-%Y %H:%M:%S'). Error: $awsoutput" | tee -a /tmp/kubernetes-s3-mysql-backup.log
+            echo -e "Database backup failed to upload for $CURRENT_DATABASE at $(TZ='Asia/Seoul' date +'%Y-%m-%d %H:%M:%S'). Error: $awsoutput" | tee -a /tmp/kubernetes-s3-mysql-backup.log
             has_failed=true
         fi
 
     else
-        echo -e "Database backup FAILED for $CURRENT_DATABASE at $(date +'%d-%m-%Y %H:%M:%S'). Error: $sqloutput" | tee -a /tmp/kubernetes-s3-mysql-backup.log
+        echo -e "Database backup FAILED for $CURRENT_DATABASE at $(TZ='Asia/Seoul' date +'%Y-%m-%d %H:%M:%S'). Error: $sqloutput" | tee -a /tmp/kubernetes-s3-mysql-backup.log
         has_failed=true
     fi
 
